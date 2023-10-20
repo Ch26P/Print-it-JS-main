ab045c3ed4c89a85ae8176348d63152dbc0a12e5
Pochon Christophe <chris26-78@hotmail.fr>
2023-10-20 12:40:50 +0200

    main.js modifier

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,12 +1,7 @@
-//creation d une variable compteur
-let compteur=0
-console.log(compteur)
-//recuperation imag de fond
-let imagefondcarrousel = document.querySelector(".banner-img ")
-//creation d une variable du nbrs d'image
-let nbrsimgs=slides.length
-console.log(nbrsimgs)
 
+let compteur=0  //creation d une variable compteur
+let imagefondcarrousel = document.querySelector(".banner-img ")//recuperation imag de fond
+let nbrsimgs=slides.length//creation d une variable du nbrs d'image
 let insertagline= document.querySelector("#banner p ")
 insertagline.innerHTML = slides[compteur]["tagLine"]
 
@@ -21,54 +16,37 @@ for(let nbrDot=0;nbrDot<nbrsimgs;nbrDot++){
     // Récupérer un élément parent existant
     let divDot = document.querySelector(".dots ")
     // Ajouter le nouvel élément au parent
-    divDot.appendChild(nouveauInput)
-    console.log(nouveauInput);
+    divDot.appendChild(nouveauInput);
 }
 
-//recuperation des input
-let InputPresent = document.querySelectorAll("#banner input")
-
-InputPresent[compteur].classList.add("dot_selected")
+let InputPresent = document.querySelectorAll("#banner input")//recuperation des input
+InputPresent[compteur].classList.add("dot_selected")//Ajoute d'une class
 let InputSelect =InputPresent [compteur]
-//recuperation des 2 fleches
-let arrowcaroussel = document.querySelectorAll("#banner .arrow");
-//ecouter clic sur les fleches + incredenté le compteur
-arrowcaroussel[0].addEventListener("click", () => {
-    compteur--
-    if (compteur<0){
-      compteur=nbrsimgs-1
-    }
-    console.log("Vous avez cliqué sur le bouton gauche")
-    console.log(compteur)
-    imagefondcarrousel.setAttribute("src", slides[compteur]["image"])
-    console.log(InputPresent[compteur])
-    console.log(InputSelect)
-    //ajouter une class a l input selectionner
-InputPresent[compteur].classList.add("dot_selected")
-    //retirer une class a l input selectionner
-InputSelect.classList.remove("dot_selected")
-InputSelect=InputPresent[compteur]
-insertagline.innerHTML = slides[compteur]["tagLine"]//insertion tagline
-});
-    
-arrowcaroussel[1].addEventListener("click", () => {
-    compteur++
-    if (compteur>nbrsimgs-1){
-      compteur=0
-    }
-    console.log("Vous avez cliqué sur le bouton droit")
-  console.log(compteur)
-  imagefondcarrousel.setAttribute("src", slides[compteur]["image"])
- 
-  InputPresent[compteur].classList.add("dot_selected")
-  InputSelect.classList.remove("dot_selected")
-  InputSelect=InputPresent[compteur]
-  insertagline.innerHTML = slides[compteur]["tagLine"]
-});
+let arrowcaroussel = document.querySelectorAll("#banner .arrow");//recuperation des 2 fleches
 
-
-
-
+for (let i=0; i<arrowcaroussel.length;i++){
+    let arrowclick=arrowcaroussel[i]
+    arrowclick.addEventListener("click",(event)=>{
+      arrow=event.target
+      if (arrow===arrowcaroussel[0]){
+        compteur--
+        if (compteur<0){
+          compteur=nbrsimgs-1
+        }
+      }
+      if (arrow===arrowcaroussel[1]){
+        compteur++
+        if (compteur>nbrsimgs-1){
+          compteur=0
+        }
+      }
+      imagefondcarrousel.setAttribute("src", slides[compteur]["image"])//changer src de l'image
+      InputPresent[compteur].classList.add("dot_selected")//Ajoute d'une class
+      InputSelect.classList.remove("dot_selected")//Suppression d'une class
+      InputSelect=InputPresent[compteur]//
+      insertagline.innerHTML = slides[compteur]["tagLine"]//changer la tag-line
+    })
+}
 
 
 
